c0ac09540fa367ad14a4c841ac91a6d67918413d
Heitor Machado Aaujo <pedroaraujo@morethanreal.io>
2023-01-30 08:59:17 -0500

    res headers

--- server.js
+++ server.js
@@ -7,18 +7,18 @@ const connectDB = require('./db')
 connectDB()
 
 const app = express()
-
 app.use(cors())
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    next()
+})
+
 app.use('/api/characters', require('./routes'))
+
 
 app.listen(port, () => console.log(`Server Started ${port}`))
 
