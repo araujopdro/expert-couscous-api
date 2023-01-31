da0134f2b24761cfa6ef9403cc3e3eaaab03f7de
Heitor Machado Aaujo <pedroaraujo@morethanreal.io>
2023-01-30 21:27:27 -0500

    removed db

--- server.js
+++ server.js
@@ -3,9 +3,6 @@ const cors = require('cors')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 3000
 
-const connectDB = require('./db')
-connectDB()
-
 const app = express()
 
 app.use(cors())
@@ -15,19 +12,5 @@ console.log("cors")
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-console.log("routes")
-app.use('/api/characters', require('./routes'))
-
-
 app.listen(port, () => console.log(`Server Started ${port}`))
 
-const errorHandler = (err, req, res, next) => {
-    const statusCode = res.statusCode ? res.statusCode : 500
-    res.status(statusCode)
-    res.json({
-        message: err.message,
-        stack: process.env.NODE_ENV === 'production' ? null : err.stack
-    })
-}
-
-app.use(errorHandler)
