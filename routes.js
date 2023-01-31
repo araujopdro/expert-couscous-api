950d5d2526bb094bc6c907e3371bcef76f77f128
Heitor Machado Aaujo <pedroaraujo@morethanreal.io>
2023-01-30 21:00:44 -0500

    get /

--- routes.js
+++ routes.js
@@ -1,9 +1,5 @@
 const express = require('express')
-const cors = require('cors')
-
 const router = express.Router()
-router.use(cors())
-console.log("router cors")
 const { getCharacters, createCharacter, updateCharacter, deleteCharacter } = require('./controller.js');
 
 router.route('/').get(getCharacters).post(createCharacter)
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 3000
 
@@ -6,15 +7,28 @@ const connectDB = require('./db')
 connectDB()
 
 const app = express()
+app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    next()
+})
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: "resposta"})
+})
 
 app.use('/api/characters', require('./routes'))
 
 
 app.listen(port, () => console.log(`Server Started ${port}`))
 
+
+//////Error///////
 const errorHandler = (err, req, res, next) => {
     const statusCode = res.statusCode ? res.statusCode : 500
     res.status(statusCode)
