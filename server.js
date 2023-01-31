c440af49a45e49c1c73dc61f9ba1a5c738099c58
Heitor Machado Aaujo <pedroaraujo@morethanreal.io>
2023-01-30 21:18:09 -0500

    cors

--- server.js
+++ server.js
@@ -7,28 +7,20 @@ const connectDB = require('./db')
 connectDB()
 
 const app = express()
+
 app.use(cors())
+
+console.log("cors")
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-    next()
-})
-
-app.get('/api/test', (req, res) => {
-    res.status(200).json({message: "resposta"})
-})
-
-app.use('/api/teste', require('./routes'))
+console.log("routes")
+app.use('/api/characters', require('./routes'))
 
 
 app.listen(port, () => console.log(`Server Started ${port}`))
 
-
-//////Error///////
 const errorHandler = (err, req, res, next) => {
     const statusCode = res.statusCode ? res.statusCode : 500
     res.status(statusCode)
